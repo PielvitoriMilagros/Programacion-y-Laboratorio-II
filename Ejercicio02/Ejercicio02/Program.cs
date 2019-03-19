2c5ea61fada0354cd56fddb606f3d504c8fcf14b
PielvitoriMilagros <miledupi@hotmail.com>
2019-03-19 19:46:31 -0300

    Add files via upload

---
+++ Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio02
+{
+    class Ejercicio_02
+    {
+        static double numero;
+        static double cuadrado;
+        static double cubo;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            while (double.TryParse(Console.ReadLine(), out numero) != true || numero <= 0)
+            {
+                Console.WriteLine("ERROR ¡Reingresar numero! ");
+            }
+            cuadrado=Math.Pow(numero, 2);
+            cubo=Math.Pow(numero, 3);
+            Console.WriteLine("El cuadrado de {0} es {1}",numero,cuadrado);
+            Console.WriteLine("El cubo de {0} es {1}", numero, cubo);
+
+            Console.ReadKey();
+
+        }
+    }
+}
