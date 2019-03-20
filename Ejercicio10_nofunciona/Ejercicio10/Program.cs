b35d17ffc9a9ddea87c4afa25619e15a5542c7e0
PielvitoriMilagros <miledupi@hotmail.com>
2019-03-19 22:19:16 -0300

    Add files via upload

---
+++ Ejercicio10_nofunciona/Ejercicio10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        public static int numero;
+        public static int min;
+        public static int max;
+        public static int cont = 0;
+        static void Main(string[] args)
+        {
+            for (int x = 1; x < 5; x++)
+            {
+                Console.WriteLine("Ingrese un numero entre -100 y 100");
+                //            while(!int.TryParse(Console.ReadLine(),out numero))
+                numero = int.Parse(Console.ReadLine());
+                while (!Validacion.Validar(numero, -100, 100))
+                {
+                    Console.WriteLine("ERROR reingrese numero");
+                }
+                cont++;
+                if (cont == 1)
+                {
+                    min = numero;
+                    max = numero;
+                }
+                else
+                {
+                    if (numero < min)
+                        min = numero;
+                    if (numero > max)
+                        max = numero;
+                }
+            }
+            Console.WriteLine("El mayor fue " + max);
+            Console.WriteLine("El menor fue " + min);
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Ejercicio10_nofunciona/Ejercicio10/Validacion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    public class Validacion
+    {
+        public static bool retorno;
+        public static int cont = 0;
+        public static bool Validar(int valor, int min, int max)
+        {
+            retorno = true;
+            if (int.TryParse(Console.ReadLine(), out valor) != true)
+                retorno = false;
+            else
+            {
+                if (valor > max || valor < min)
+                {
+                    /*                    cont++;
+                                        if(cont==1)
+                                        {
+                                            min = valor;
+                                            max = valor;
+                                        }
+                                        else
+                                        {
+                                            if (valor < min)
+                                                min = valor;
+                                            if (valor > max)
+                                                max = valor;
+                    */
+                    //                } else
+                    //                {
+                    retorno = false;
+                }
+            }
+            return retorno;
+        }
+    }
+}
