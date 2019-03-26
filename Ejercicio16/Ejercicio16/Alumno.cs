86b4b16bf5076780aa283286441a709d43781988
PielvitoriMilagros <miledupi@hotmail.com>
2019-03-25 21:33:57 -0300

    Add files via upload

---
+++ Ejercicio16/Ejercicio16/Alumno.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    public class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public static string colegio;
+        static Random rnd;
+
+        // Constructor con parámetros
+        public Alumno(string apellido, string nombre)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        // Constructor para los atributos static
+        static Alumno()
+        {
+            colegio = "UTN";
+            rnd = new Random();
+        }
+
+
+
+        public void CalcularFinal()
+        {
+            if(this.nota1 >= 4 && this.nota2>=4)
+            {
+                this.notaFinal = rnd.Next();
+            } else
+            {
+                this.notaFinal = -1;
+            }
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+
+        public string Mostrar()
+        {
+//            CONDICION? VERDADERO:FALSO
+
+            return this.legajo.ToString() + "-" + this.apellido + " " +
+                this.nombre + " " + colegio + " " +
+                (this.notaFinal == -1 ? "DESAPROBADO" : this.notaFinal.ToString());
+
+            /*            if(this.notaFinal == -1)
+                        {
+                            return this.nombre + " " + this.apellido + " " + this.nota1 + " " + this.nota2 + " " + "DESAPROBADO"+" "+colegio;
+                        }
+                        else
+                        {
+                            return this.nombre + " " + this.apellido + " " + this.nota1 + " " + this.nota2+" "+this.notaFinal+" "+colegio;
+                        }
+            */
+
+        }
+    }
+}
---
+++ Ejercicio16/Ejercicio16/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Perez", "Juancito");
+            Alumno alumno2 = new Alumno("Garcia", "Pedrito");
+            Alumno alumno3 = new Alumno("Juarez", "Taquito");
+
+
+            /* Sin constructor
+             * Alumno.colegio = "UTN";
+
+             * Una vez que se crea el constructor, no es necesario ponerlo siquiera aca
+             */
+
+
+            // Sin constructor
+            //            Alumno alumno1 = new Alumno();
+            //          alumno1.nombre = "Juancito";
+            //          alumno1.apellido = "Perez";
+            alumno1.legajo = 123456;
+            alumno1.Estudiar(8, 7);
+            alumno1.CalcularFinal();
+            Console.WriteLine(alumno1.Mostrar());
+
+            // Sin constructor
+            //            Alumno alumno2 = new Alumno();
+            //            alumno2.nombre = "Pedrito";
+            //            alumno2.apellido = "Garcia";
+            alumno2.legajo = 654321;
+            alumno2.Estudiar(7, 5);
+            alumno2.CalcularFinal();
+            Console.WriteLine(alumno2.Mostrar());
+
+            // Sin constructor
+            //           Alumno alumno3 = new Alumno();
+            //           alumno3.nombre = "Taquito";
+            //           alumno3.apellido = "Juarez";
+            alumno3.legajo = 789456;
+            alumno3.Estudiar(2, 3);
+            alumno3.CalcularFinal();
+            Console.WriteLine(alumno3.Mostrar());
+
+
+            Console.ReadKey();
+        }
+    }
+}
