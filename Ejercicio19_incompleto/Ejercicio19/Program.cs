576f93c6792a24079fec7b19e15bcf065e102a84
PielvitoriMilagros <miledupi@hotmail.com>
2019-03-26 22:00:51 -0300

    Add files via upload

---
+++ Ejercicio19_incompleto/Ejercicio19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador = new Sumador();
+
+            Console.WriteLine("Suma int");
+            Console.WriteLine(sumador.Sumar(15, 32) +" "+ sumador.GetCantSumas());
+
+            Console.WriteLine("Suma string");
+            Console.WriteLine(sumador.Sumar("Hola", "Mundito")+" "+sumador.GetCantSumas());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Ejercicio19_incompleto/Ejercicio19/Sumador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio19
+{
+    class Sumador
+    {
+        private int cantidadSumas;
+
+
+        public int GetCantSumas()
+        {
+            return this.cantidadSumas;
+
+        }
+
+        public Sumador(int cant)
+        {
+            this.cantidadSumas = cant;
+        }
+
+        public Sumador() : this(0)
+        {
+
+        }
+
+        public long Sumar(long a, long b)
+        {
+            long result;
+            this.cantidadSumas += 1;
+            result = a + b;
+            return result;
+        }
+
+        public string Sumar(string a,string b)
+        {
+            string result;
+            this.cantidadSumas += 1;
+            result = a + b;
+            return result;
+        }
+    }
+}
