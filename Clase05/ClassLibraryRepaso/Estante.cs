5405d0e63ab66e63463522f4ec4ce5bef0ad001d
PielvitoriMilagros <miledupi@hotmail.com>
2019-04-01 22:08:57 -0300

    Add files via upload

---
+++ Clase05/ClassLibraryRepaso/Estante.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryRepaso
+{
+    public class Estante
+    {
+        //Atributos
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        //Constructores
+
+        //Así inicializa un array, viene de la clase así que hay que hacer un new
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        // Así reutilizo un constructor
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+
+        //Propiedad
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            string salida = e.ubicacionEstante + " ";
+
+            // Producto producto, es una variable auxiliar del tipo Producto para ir leyendo cada e.productos
+            foreach (Producto producto in e.productos)
+            {
+                if (!(producto is null))
+                    salida += Producto.MostrarProducto(producto);
+            }
+            /*
+             for (int i=0; i<e.productos.Length; i++)
+             {
+                Producto producto = e.productos[i];
+             }
+             */
+            return salida;
+        }
+
+        //Sobrecarga de operadores
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach (Producto producto in e.productos)
+            {
+                if (!(producto is null) && !(p is null) && producto == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        //Sobrecarga de + y -
+        public static bool operator +(Estante e,Producto p)
+        {
+            if (e!=p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if(e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        public static Estante operator -(Estante e,Producto p)
+        {
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if(e.productos[i] == p)
+                {
+                    e.productos[i] = null;
+                    i = e.productos.Length;
+                }
+            }
+            return e;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Clase05/ClassLibraryRepaso/Producto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryRepaso
+{
+    public class Producto
+    {
+        //Atributos
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        //Constructor de instancia
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        //Propiedades
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        //Métodos
+        public static string MostrarProducto(Producto p)
+        {
+            //            return p.marca + " " + p.codigoDeBarra + " " + p.precio.ToString();
+            return "Codigo de barra: " + (string)p + " Marca: " + p.GetMarca() + " Precio: " + p.GetPrecio() + "\n";
+        }
+
+        //Sobrecarga explicita
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        //Sobrecarga con dos objetos
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            /*            if (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+                            return true;
+                        return false;
+             */
+            //No se necesita el IF porque está implicito en la condición. Si se cumple eso, y a me va a devolver true
+            return ((string)p1 == (string)p2 && p1.GetMarca() == p2.GetMarca());
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        //Sobrecarga con objeto y string
+        public static bool operator ==(Producto p1, string marca)
+        {
+            /*            if (p1.marca == marca)
+                            return true;
+                        return false;
+             */
+            return p1.marca == marca;
+    }
+
+        public static bool operator !=(Producto p1, string marca)
+        {
+            return !(p1.marca == marca);
+        }
+
+
+
+    }
+}
