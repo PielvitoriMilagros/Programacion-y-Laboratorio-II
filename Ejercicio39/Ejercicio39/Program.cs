0e87d8a3de4b9a4dedf4b75fb3978ebc84e582b1
PielvitoriMilagros <miledupi@hotmail.com>
2019-04-23 22:01:15 -0300

    Add files via upload

---
+++ Ejercicio39/Ejercicio39/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       Console.Title = "Ejercicio Nº38 Guía 2017";
+                        Sobreescrito sobrecarga = new SobreSobreescrito();
+                        Console.WriteLine(sobrecarga.ToString());
+                        //            Sobreescrito comparador = new Sobreescrito();
+                        string objeto = "¡Este es mi método ToString sobreescrito!";
+                        Console.WriteLine("----------------------------------------------");
+                        Console.Write("Comparación Sobrecargas con String: ");
+                        Console.WriteLine(sobrecarga.Equals(objeto));
+                        Console.WriteLine("----------------------------------------------");
+                        Console.WriteLine(sobrecarga.GetHashCode());
+                        
+            Sobreescrito prueba = new SobreSobreescrito();
+            Console.WriteLine(prueba.MiMetodo());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio39/Ejercicio39/Sobreescrito.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio39
+{
+    public abstract class Sobreescrito
+    {
+        protected string miAtributo;
+
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar Abstractos";
+        }
+
+        public abstract string MiPropiedad { get; }
+
+        public abstract string MiMetodo();
+
+
+
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+            //            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Sobreescrito)
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+
+    }
+}
