0c53889aa37c53f2713477ebb8f769c29d6fbfd5
PielvitoriMilagros <miledupi@hotmail.com>
2019-04-23 21:35:55 -0300

    Add files via upload

---
+++ Ejercicio38/Ejercicio38/Sobreescrito.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio38
+{
+    public class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+            //            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Sobreescrito)
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+
+    }
+}
