107e39a4d9bd1fc334dea6646dca8db1703ca273
PielvitoriMilagros <miledupi@hotmail.com>
2019-05-20 20:48:00 -0300

    Add files via upload

---
+++ Ejercicio48/Ejercicio48/Contabilidad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio48
+{
+    public class Contabilidad<T, U> where T : Documento
+        where U : Documento, new()
+    {
+        List<T> egresos;
+        List<U> ingresos;
+
+        public static Contabilidad<T,U> operator +(Contabilidad<T, U> c, T egreso)
+        {
+            c.egresos.Add(egreso);
+            return c;
+        }
+
+        public static Contabilidad<T, U> operator +(Contabilidad<T, U> c, U ingreso)
+        {
+            c.ingresos.Add(ingreso);
+            return c;
+        }
+
+    }
+}
---
+++ Ejercicio48/Ejercicio48/Recibo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio48
+{
+    class Recibo : Documento
+    {
+        public Recibo() : base(0) { }
+        public Recibo(int numero) : base(numero) { }
+
+
+
+    }
+}
