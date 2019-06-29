83886946080a6d68d9a396324e49a89575151444
pielvitorimilagros <miledupi@hotmail.com>
2019-06-29 14:08:43 -0300

    agrego el tp como la gente

---
+++ Pielvitori.Milagros.2D.TP3/UnitTestProject1/TestExcepciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Excepciones;
+using Clases_Abstractas;
+using Clases_Instanciables;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestExcepciones
+    {
+        [TestMethod]
+        public void ExcepcionAlumnoRepetido()
+        {
+            try
+            {
+                Universidad univ = new Universidad();
+                Alumno a1 = new Alumno(1, "Juan", "Perez", "45698", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+                univ += a1;
+                univ += a1;
+
+            }
+            catch (Exception e)
+            {
+
+                Assert.IsInstanceOfType(e, typeof(AlumnoRepetidoException));
+            }
+        }
+    }
+}
---
+++ Pielvitori.Milagros.2D.TP3/UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clases_Instanciables;
+using Excepciones;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
